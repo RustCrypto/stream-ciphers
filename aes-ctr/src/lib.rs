3259a16867836ab7259d5718f423d8d6edd02ddc
newpavlov <newpavlov@gmail.com>
2018-07-24 15:27:57 +0300

    reformatting

--- aes-ctr/src/lib.rs
+++ aes-ctr/src/lib.rs
@@ -1,17 +1,32 @@
 #![no_std]
 pub extern crate stream_cipher;
-#[cfg(not(all(target_feature = "aes", target_feature = "sse2", target_feature = "ssse3", any(target_arch = "x86_64", target_arch = "x86"))))]
+#[cfg(not(all(
+    target_feature = "aes", target_feature = "sse2", target_feature = "ssse3",
+    any(target_arch = "x86_64", target_arch = "x86"),
+)))]
 extern crate ctr;
-#[cfg(not(all(target_feature = "aes", target_feature = "sse2", target_feature = "ssse3", any(target_arch = "x86_64", target_arch = "x86"))))]
+#[cfg(not(all(
+    target_feature = "aes", target_feature = "sse2", target_feature = "ssse3",
+    any(target_arch = "x86_64", target_arch = "x86"),
+)))]
 extern crate aes_soft;
 
-#[cfg(all(target_feature = "aes", target_feature = "sse2", target_feature = "ssse3", any(target_arch = "x86_64", target_arch = "x86")))]
-extern crate aesni;
-
-#[cfg(not(all(target_feature = "aes", target_feature = "sse2", target_feature = "ssse3", any(target_arch = "x86_64", target_arch = "x86"))))]
+#[cfg(not(all(
+    target_feature = "aes", target_feature = "sse2", target_feature = "ssse3",
+    any(target_arch = "x86_64", target_arch = "x86"),
+)))]
 mod dummy;
 
-#[cfg(all(target_feature = "aes", target_feature = "sse2", target_feature = "ssse3", any(target_arch = "x86_64", target_arch = "x86")))]
+#[cfg(all(
+    target_feature = "aes", target_feature = "sse2", target_feature = "ssse3",
+    any(target_arch = "x86_64", target_arch = "x86"),
+))]
+extern crate aesni;
+
+#[cfg(all(
+    target_feature = "aes", target_feature = "sse2", target_feature = "ssse3",
+    any(target_arch = "x86_64", target_arch = "x86"),
+))]
 use aesni as dummy;
 
 pub use dummy::{Aes128Ctr, Aes192Ctr, Aes256Ctr};
