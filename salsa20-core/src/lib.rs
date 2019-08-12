4e37486bae784fc4d8be6bfc02fcca0c870d151c
Tony Arcieri <bascule@gmail.com>
2019-08-12 14:37:04 -0700

    salsa20-core: Remove extraneous 'extern crate std' import
    Nothing in the crate uses `std`, so all this does is break linking on
    `no_std` platforms.

--- salsa20-core/src/lib.rs
+++ salsa20-core/src/lib.rs
@@ -1,10 +1,10 @@
 #![no_std]
+
 extern crate block_cipher_trait;
 
 #[cfg(cargo_feature = "zeroize")]
 extern crate zeroize;
 
-pub extern crate std;
 pub extern crate stream_cipher;
 
 mod salsa_family_state;
