1f542cf3e3817564d47ba977d545c186dc6941e5
Artyom Pavlov <newpavlov@gmail.com>
2020-08-13 13:38:30 +0300

    Use SVG logo and update lock file (#159)

--- aes-ctr/src/lib.rs
+++ aes-ctr/src/lib.rs
@@ -38,6 +38,10 @@
 //! ```
 
 #![no_std]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
+    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg"
+)]
 #![doc(html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo_small.png")]
 #![warn(missing_docs, rust_2018_idioms)]
 
--- cfb8/src/lib.rs
+++ cfb8/src/lib.rs
@@ -44,7 +44,10 @@
 //! [2]: https://en.wikipedia.org/wiki/Stream_cipher#Self-synchronizing_stream_ciphers
 
 #![no_std]
-#![doc(html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo_small.png")]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
+    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg"
+)]
 #![deny(unsafe_code)]
 #![warn(missing_docs, rust_2018_idioms)]
 
