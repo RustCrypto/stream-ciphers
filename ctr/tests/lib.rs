e6f481853cd4dd716504d21e38553153d7c3b441
Tony Arcieri <bascule@gmail.com>
2020-10-14 12:11:14 -0700

    ctr: refactor tests (#175)

--- ctr/tests/lib.rs
+++ ctr/tests/lib.rs
@@ -1,7 +1,4 @@
-type Aes128Ctr = ctr::Ctr128<aes::Aes128>;
-type Aes256Ctr = ctr::Ctr128<aes::Aes256>;
+//! Counter Mode Tests
 
-stream_cipher::new_sync_test!(aes128_ctr_core, Aes128Ctr, "aes128-ctr");
-stream_cipher::new_sync_test!(aes256_ctr_core, Aes256Ctr, "aes256-ctr");
-stream_cipher::new_seek_test!(aes128_ctr_seek, Aes128Ctr);
-stream_cipher::new_seek_test!(aes256_ctr_seek, Aes256Ctr);
+mod ctr128;
+mod ctr32;
