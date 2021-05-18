c10e44aa7c40c92c2f3ed9a7ba93955012c193e4
seanybaggins <slink@syncroness.com>
2021-05-18 09:30:28 -0600

    Added rfc7539-section-242 test

--- chacha20/tests/lib.rs
+++ chacha20/tests/lib.rs
@@ -207,6 +207,53 @@ mod overflow {
     }
 }
 
+#[rustfmt::skip]
+mod chacha20_tests {
+    use chacha20::{ChaCha20, Key, Nonce};
+    use cipher::{NewCipher, StreamCipher};
+    use hex_literal::hex;
+
+    #[test]
+    // https://datatracker.ietf.org/doc/html/rfc7539#section-2.4.2
+    fn rfc7539_section_2_4_2() {
+        let mut data = hex!("
+            4c 61 64 69 65 73 20 61 6e 64 20 47 65 6e 74 6c  
+            65 6d 65 6e 20 6f 66 20 74 68 65 20 63 6c 61 73  
+            73 20 6f 66 20 27 39 39 3a 20 49 66 20 49 20 63  
+            6f 75 6c 64 20 6f 66 66 65 72 20 79 6f 75 20 6f  
+            6e 6c 79 20 6f 6e 65 20 74 69 70 20 66 6f 72 20  
+            74 68 65 20 66 75 74 75 72 65 2c 20 73 75 6e 73  
+            63 72 65 65 6e 20 77 6f 75 6c 64 20 62 65 20 69  
+            74 2e
+        ");
+        let key = Key::from(hex!(
+            "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f"
+        ));
+        let nonce = Nonce::from(hex!("000000000000004a00000000"));
+        let mut cipher = ChaCha20::new(&key, &nonce);
+
+        // Test from the rfc requires that the counter be 1. Must use the consume the first 64
+        // bytes of the block since the chacha20 api starts the counter at 0
+        let mut prefix = [0u8; 64];
+        cipher.apply_keystream(&mut prefix);
+
+        cipher.apply_keystream(&mut data);
+
+        let expected_data = hex!("
+            6e 2e 35 9a 25 68 f9 80 41 ba 07 28 dd 0d 69 81 
+            e9 7e 7a ec 1d 43 60 c2 0a 27 af cc fd 9f ae 0b  
+            f9 1b 65 c5 52 47 33 ab 8f 59 3d ab cd 62 b3 57  
+            16 39 d6 24 e6 51 52 ab 8f 53 0c 35 9f 08 61 d8  
+            07 ca 0d bf 50 0d 6a 61 56 a3 8e 08 8a 22 b6 5e  
+            52 bc 51 4d 16 cc f8 06 81 8c e9 1a b7 79 37 36  
+            5a f9 0b bf 74 a3 5b e6 b4 0b 8e ed f2 78 5e 42  
+            87 4d
+        ");
+
+        assert_eq!(expected_data, data)
+    }
+}
+
 #[cfg(feature = "xchacha")]
 #[rustfmt::skip]
 mod xchacha20 {
