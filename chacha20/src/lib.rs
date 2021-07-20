d614a3b1882ab1cf5ba175d9eef3ccc2ceb59c1a
Artyom Pavlov <newpavlov@gmail.com>
2021-07-20 14:58:41 +0000

    use consistent doc attribute across crates (#258)

--- chacha20/src/lib.rs
+++ chacha20/src/lib.rs
@@ -69,8 +69,9 @@
 
 #![no_std]
 #![doc(
-    html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
-    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg"
+    html_logo_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg",
+    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg",
+    html_root_url = "https://docs.rs/chacha20/0.7.2"
 )]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![warn(missing_docs, rust_2018_idioms, trivial_casts, unused_qualifications)]
