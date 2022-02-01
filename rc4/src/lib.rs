3e2f58b6870077e1e575c0af194d5760ed937fc4
magic-akari <akari.ccino@gmail.com>
2022-02-01 19:22:32 +0800

    chore: try implement NewCipher

--- rc4/src/lib.rs
+++ rc4/src/lib.rs
@@ -11,51 +11,61 @@
 
 pub use cipher;
 
-use cipher::{
-    errors::{InvalidLength, LoopError},
-    StreamCipher,
-};
+use cipher::{consts::U0, errors::LoopError, generic_array::ArrayLength, NewCipher, StreamCipher};
 
+use core::marker::PhantomData;
 #[cfg(cargo_feature = "zeroize")]
 use std::ops::Drop;
 #[cfg(cargo_feature = "zeroize")]
 use zeroize::Zeroize;
 
 /// The RC-4 stream cipher
-pub struct Rc4 {
+pub struct Rc4<KeySize> {
     state: [u8; 256],
     i: u8,
     j: u8,
+
+    key_size: PhantomData<KeySize>,
 }
 
-impl Rc4 {
-    /// Create new stream cipher instance from variable length key and nonce
-    /// given as byte slices.
-    pub fn new_from_slices(key: &[u8]) -> Result<Self, InvalidLength> {
-        if key.is_empty() || key.len() > 256 {
-            return Err(InvalidLength);
-        }
+impl<KeySize> NewCipher for Rc4<KeySize>
+where
+    KeySize: ArrayLength<u8>,
+{
+    type NonceSize = U0;
+    type KeySize = KeySize;
+
+    fn new(key: &cipher::CipherKey<Self>, _nonce: &cipher::Nonce<Self>) -> Self {
+        assert!(key.len() >= 1 && key.len() < 256);
 
         let mut rc4 = Rc4 {
             state: [0; 256],
             i: 0,
             j: 0,
+
+            key_size: Default::default(),
         };
 
         rc4.ksa(key);
 
-        Ok(rc4)
+        rc4
     }
 }
 
-impl StreamCipher for Rc4 {
+impl<KeySize> StreamCipher for Rc4<KeySize>
+where
+    KeySize: ArrayLength<u8>,
+{
     fn try_apply_keystream(&mut self, data: &mut [u8]) -> Result<(), LoopError> {
         self.process(data);
         Ok(())
     }
 }
 
-impl Rc4 {
+impl<KeySize> Rc4<KeySize>
+where
+    KeySize: ArrayLength<u8>,
+{
     fn ksa(&mut self, key: &[u8]) {
         self.state.iter_mut().enumerate().for_each(|(i, x)| {
             *x = i as u8;
@@ -117,7 +127,18 @@ impl Drop for Rc4 {
 
 #[test]
 fn test() {
-    let mut rc4 = Rc4::new_from_slices(b"Key").unwrap();
+    let mut rc4 = Rc4::new(b"Key".into(), &Default::default());
+
+    let mut data = b"Plaintext".to_vec();
+    rc4.try_apply_keystream(&mut data).unwrap();
+    assert_eq!(data, [0xBB, 0xF3, 0x16, 0xE8, 0xD9, 0x40, 0xAF, 0x0A, 0xD3]);
+}
+
+#[test]
+fn test2() {
+    use cipher::consts::U3; 
+
+    let mut rc4 = Rc4::<U3>::new_from_slices(b"Key", Default::default()).unwrap();
 
     let mut data = b"Plaintext".to_vec();
     rc4.try_apply_keystream(&mut data).unwrap();
