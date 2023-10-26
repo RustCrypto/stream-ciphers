aac6b9ede61db8f7227a66018f08695f7bc723c0
Noah Stiltner <nstilt1@lsu.edu>
2023-10-25 19:37:23 -0500

    add chacha20rng bench code

--- benches/src/chacha20.rs
+++ benches/src/chacha20.rs
@@ -28,9 +28,49 @@ fn bench(c: &mut Criterion<CyclesPerByte>) {
     group.finish();
 }
 
+use chacha20::{
+    ChaCha20Rng, rand_core::{SeedableRng, RngCore}
+};
+fn bench_chacha20rng(c: &mut Criterion<CyclesPerByte>) {
+    let mut group = c.benchmark_group("chacha20-ChaCha20Rng");
+
+    for size in &[KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB] {
+        let mut buf = vec![0u8; *size];
+
+        group.throughput(Throughput::Bytes(*size as u64));
+
+        group.bench_function(BenchmarkId::new("fill_bytes", size), |b| {
+            let mut rng = ChaCha20Rng::from_seed([0u8; 32]);
+            b.iter(|| rng.fill_bytes(&mut buf));
+        });
+    }
+
+    group.finish();
+
+    
+    let mut original = c.benchmark_group("c2-chacha-ChaCha20Rng");
+
+    for size in &[KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB] {
+        let mut buf = vec![0u8; *size];
+        original.throughput(Throughput::Bytes(*size as u64));
+
+        original.bench_function(BenchmarkId::new("fill_bytes", size), |b| {
+            let mut rng = rand_chacha::ChaCha20Rng::from_seed([0u8; 32]);
+            b.iter(|| rng.fill_bytes(&mut buf));
+        });
+    }
+    original.finish();
+}
+
+criterion_group!(
+    name = benches_chacha20rng;
+    config = Criterion::default().with_measurement(CyclesPerByte);
+    targets = bench_chacha20rng
+);
+
 criterion_group!(
     name = benches;
     config = Criterion::default().with_measurement(CyclesPerByte);
     targets = bench
 );
-criterion_main!(benches);
+criterion_main!(benches, benches_chacha20rng);
