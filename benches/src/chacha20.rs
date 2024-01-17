0295f7cd48ef259f3f24ef6b7c61e0c64248f74a
Noah Stiltner <nstilt1@lsu.edu>
2024-01-17 16:38:56 -0600

    cleanup /benches/; also, .cargo/config has stopped working on my end

--- benches/src/chacha20.rs
+++ benches/src/chacha20.rs
@@ -1,8 +1,6 @@
 //! ChaCha20 benchmark
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
-
-#[cfg(any(target_arch = "x86_64", target_arch = "x86"))]
-use criterion_cycles_per_byte::CyclesPerByte;
+use benches::{criterion_group_bench, Benchmarker};
 
 use chacha20::{
     cipher::{KeyIvInit, StreamCipher},
@@ -10,28 +8,7 @@ use chacha20::{
 };
 
 const KB: usize = 1024;
-#[cfg(any(target_arch = "x86_64", target_arch = "x86"))]
-fn bench(c: &mut Criterion<CyclesPerByte>) {
-    let mut group = c.benchmark_group("stream-cipher");
-
-    for size in &[KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB] {
-        let mut buf = vec![0u8; *size];
-
-        group.throughput(Throughput::Bytes(*size as u64));
-
-        group.bench_function(BenchmarkId::new("apply_keystream", size), |b| {
-            let key = Default::default();
-            let nonce = Default::default();
-            let mut cipher = ChaCha20::new(&key, &nonce);
-            b.iter(|| cipher.apply_keystream(&mut buf));
-        });
-    }
-
-    group.finish();
-}
-
-#[cfg(not(any(target_arch = "x86_64", target_arch = "x86")))]
-fn bench(c: &mut Criterion) {
+fn bench(c: &mut Benchmarker) {
     let mut group = c.benchmark_group("stream-cipher");
 
     for size in &[KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB] {
@@ -52,79 +29,30 @@ fn bench(c: &mut Criterion) {
 
 use chacha20::rand_core::{RngCore, SeedableRng};
 
-#[cfg(any(target_arch = "x86_64", target_arch = "x86"))]
-fn bench_chacha20rng(c: &mut Criterion<CyclesPerByte>) {
-    // by using the same group twice, it should allow us to see a direct comparison
-    // of both implementations
-    // it seems like it needs to be manually switched using comments
-    let mut chacha_x86 = c.benchmark_group("chacha-SIMD-comparison-x86");
-
-    // no SIMD first
+fn bench_chacha20rng(c: &mut Benchmarker) {
+    let mut group = c.benchmark_group("ChaCha20Rng");
+    
     for size in &[KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB] {
         let mut buf = vec![0u8; *size];
 
-        chacha_x86.throughput(Throughput::Bytes(*size as u64));
+        group.throughput(Throughput::Bytes(*size as u64));
 
-        chacha_x86.bench_function(BenchmarkId::new("fill_bytes", size), |b| {
+        group.bench_function(BenchmarkId::new("fill_bytes", size), |b| {
             let mut rng = chacha20::ChaCha20Rng::from_seed([0u8; 32]);
-            //let mut rng = rand_chacha::ChaCha20Rng::from_seed([0u8; 32]);
-            b.iter(|| rng.fill_bytes(&mut buf));
-            //let mut cipher = ChaCha20::new(&Default::default(), &Default::default());
-            //b.iter(|| cipher.apply_keystream(&mut buf));
-        });
-    }
-
-    chacha_x86.finish();
-}
-
-#[cfg(not(any(target_arch = "x86_64", target_arch = "x86")))]
-fn bench_chacha20rng(c: &mut Criterion) {
-    // by using the same group twice, it should allow us to see a direct comparison
-    // of both implementations
-    // it seems like it needs to be manually switched using comments
-    let mut chacha_aarch64 = c.benchmark_group("chacha-SIMD-comparison-aarch64");
-
-    // no SIMD first
-    for size in &[KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB] {
-        let mut buf = vec![0u8; *size];
-
-        chacha_aarch64.throughput(Throughput::Bytes(*size as u64));
-
-        chacha_aarch64.bench_function(BenchmarkId::new("fill_bytes", size), |b| {
-            let mut rng = chacha20::ChaCha20Rng::from_seed([0u8; 32].into());
-            //let mut rng = rand_chacha::ChaCha20Rng::from_seed([0u8; 32]);
             b.iter(|| rng.fill_bytes(&mut buf));
         });
     }
 
-    chacha_aarch64.finish();
+    group.finish();
 }
-
-#[cfg(any(target_arch = "x86_64", target_arch = "x86"))]
-criterion_group!(
-    name = benches_chacha20rng;
-    config = Criterion::default().with_measurement(CyclesPerByte);
-    targets = bench_chacha20rng
+criterion_group_bench!(
+    benches_chacha20rng,
+    bench_chacha20rng
 );
 
-#[cfg(any(target_arch = "x86_64", target_arch = "x86"))]
-criterion_group!(
-    name = benches;
-    config = Criterion::default().with_measurement(CyclesPerByte);
-    targets = bench
+criterion_group_bench!(
+    benches,
+    bench
 );
 
-#[cfg(not(any(target_arch = "x86_64", target_arch = "x86")))]
-criterion_group!(
-    name = benches_chacha20rng;
-    config = Criterion::default();
-    targets = bench_chacha20rng
-);
-
-#[cfg(not(any(target_arch = "x86_64", target_arch = "x86")))]
-criterion_group!(
-    name = benches;
-    config = Criterion::default();
-    targets = bench
-);
 criterion_main!(benches, benches_chacha20rng);
--- benches/src/lib.rs
+++ benches/src/lib.rs
@@ -1,1 +1,24 @@
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
 
+#[cfg(any(target_arch = "x86_64", target_arch = "x86", all(target_arch = "aarch64", target_os = "linux")))]
+pub type Benchmarker = Criterion<criterion_cycles_per_byte::CyclesPerByte>;
+#[cfg(not(any(target_arch = "x86_64", target_arch = "x86", all(target_arch = "aarch64", target_os = "linux"))))]
+pub type Benchmarker = Criterion;
+
+#[macro_export]
+macro_rules! criterion_group_bench {
+    ($Name:ident, $Target:ident) => {
+        #[cfg(any(target_arch = "x86_64", target_arch = "x86", all(target_arch = "aarch64", target_os = "linux")))]
+        criterion_group!(
+            name = $Name;
+            config = Criterion::default().with_measurement(criterion_cycles_per_byte::CyclesPerByte);
+            targets = $Target
+        );
+        #[cfg(not(any(target_arch = "x86_64", target_arch = "x86", all(target_arch = "aarch64", target_os = "linux"))))]
+        criterion_group!(
+            name = $Name;
+            config = Criterion::default();
+            targets = $Target
+        );
+    }
+}
