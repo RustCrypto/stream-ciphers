97b31fee3c1d1a10187d1a556f94b97e6d191f9c
Alfredo Garcia <oxarbitrage@gmail.com>
2024-03-15 14:50:54 -0300

    remove not needed `cfg-if`
    Co-authored-by: Tony Arcieri <bascule@gmail.com>

--- salsa20/src/backends.rs
+++ salsa20/src/backends.rs
@@ -1,10 +1,3 @@
-use cfg_if::cfg_if;
-
-cfg_if! {
-    if #[cfg(any(target_arch = "x86", target_arch = "x86_64"))] {
-        pub(crate) mod sse2;
-        pub(crate) mod soft;
-    } else {
-        pub(crate) mod soft;
-    }
-}
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+pub(crate) mod sse2;
+pub(crate) mod soft;
