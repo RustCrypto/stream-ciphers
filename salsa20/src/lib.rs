b3eb4e8f2ec15851b303a5c013a32d656d74d449
Tony Arcieri <bascule@gmail.com>
2024-07-27 09:45:00 -0600

    chacha20+salsa20: cut pre.1 prereleases (#363)
    Cuts the following prereleases:
    - `chacha20` v0.10.0-pre.1
    - `salsa20` v0.11.0-pre.1

--- salsa20/src/lib.rs
+++ salsa20/src/lib.rs
@@ -70,8 +70,7 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg",
-    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg",
-    html_root_url = "https://docs.rs/salsa20/0.10.2"
+    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg"
 )]
 #![warn(missing_docs, rust_2018_idioms, trivial_casts, unused_qualifications)]
 
