885c8c103405f84fdf3062ed3599c41623617187
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2025-08-13 19:24:55 +0300

    Migrate to new version of `cipher`

--- salsa20/tests/ecrypt.rs
+++ salsa20/tests/ecrypt.rs
@@ -1,19 +1,22 @@
 use cipher::{KeyIvInit, StreamCipher, StreamCipherSeek, blobby};
 use salsa20::Salsa20;
 
-static DATA: &[u8] = include_bytes!("data/ecrypt.blb");
-
 /// ECRYPT test vectors:
 /// https://github.com/das-labor/legacy/blob/master/microcontroller-2/arm-crypto-lib/testvectors/salsa20-256.64-verified.test-vectors
 #[test]
 fn salsa20_ecrypt() {
-    let test_vectors = blobby::Blob4Iterator::new(DATA).unwrap();
-    for test_vector in test_vectors {
-        let [key, iv, pos, expected] = test_vector.unwrap();
+    blobby::parse_into_structs!(
+        include_bytes!("data/ecrypt.blb");
+        #[define_struct]
+        static TEST_VECTORS: &[
+            TestVector { key, iv, pos, expected }
+        ];
+    );
 
-        let key = key.try_into().unwrap();
-        let iv = iv.try_into().unwrap();
-        let pos = u32::from_be_bytes(pos.try_into().unwrap());
+    for tv in TEST_VECTORS {
+        let key = tv.key.try_into().unwrap();
+        let iv = tv.iv.try_into().unwrap();
+        let pos = u32::from_be_bytes(tv.pos.try_into().unwrap());
 
         let mut c = Salsa20::new(key, iv);
         c.seek(pos);
@@ -21,6 +24,6 @@ fn salsa20_ecrypt() {
         let mut buf = [0u8; 64];
         c.apply_keystream(&mut buf);
 
-        assert_eq!(buf, expected);
+        assert_eq!(buf, tv.expected);
     }
 }
