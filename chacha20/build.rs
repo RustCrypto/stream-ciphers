53cd90976cc1a1a43364e760ea6e8fcb7ae3774b
Noah Stiltner <nstilt1@lsu.edu>
2026-01-10 12:24:47 -0600

    returned serde back with a compile-time warning about using serde

---
+++ chacha20/build.rs
@@ -0,0 +1,5 @@
+fn main() {
+    if cfg!(feature = "serde") {
+        println!("cargo:warning=`serde` feature is enabled. Serializing CSPRNG states can leave unzeroizable copies of the seed in memory.");
+    }
+}
