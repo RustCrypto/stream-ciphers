10502cadce80be1d65cc1248991a749f57fe645d
Noah Stiltner <nstilt1@lsu.edu>
2026-01-10 12:38:38 -0600

    fmt

--- chacha20/build.rs
+++ chacha20/build.rs
@@ -1,5 +1,7 @@
 fn main() {
     if cfg!(feature = "serde") {
-        println!("cargo:warning=`serde` feature is enabled. Serializing CSPRNG states can leave unzeroizable copies of the seed in memory.");
+        println!(
+            "cargo:warning=`serde` feature is enabled. Serializing CSPRNG states can leave unzeroizable copies of the seed in memory."
+        );
     }
 }
